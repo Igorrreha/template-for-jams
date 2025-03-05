2caeed2c8b9029eb0f5573eab7d728554936711e
Igorrreha <igorecha.9999@gmail.com>
2025-03-05 21:09:05 +0700

    Improve hide animation, make preview starting manual

--- modules/gamestart_splashscreen/gamestart_splashscreen.gd
+++ modules/gamestart_splashscreen/gamestart_splashscreen.gd
@@ -1,8 +1,19 @@
 extends Control
 
 
+signal showing_started
 signal showing_completed
+
+
+var _is_showing_started: bool
 
 
 func complete_showing() -> void:
 	showing_completed.emit()
+
+
+func _input(event: InputEvent) -> void:
+	if not _is_showing_started\
+	and event.is_action_pressed("ui_accept"):
+		_is_showing_started = true
+		showing_started.emit()
